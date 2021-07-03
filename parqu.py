f6218d524925d82410a0316dd16e4a7a87cfe728
kenny.yeung <kenny.yeung@gmail.com>
2021-07-03 23:18:42 +0000

    cleaned up code

--- parqu.py
+++ parqu.py
@@ -1,170 +1,131 @@
 #!/usr/bin/env python3
 from pyarrow import fs as FS
-import argparse
 import pyarrow.parquet as pq
+import argparse
 import json
 import logging as logger
-from multiprocessing import Pool
-import jmespath
-import tracemalloc
-
+# from logzero import logger
+# from multiprocessing import Pool
 
 # https://parquet.apache.org/documentation/latest/
 
-# !todo: possible bug there seems f.size issue... 
-# if that is not returned properly, its not going to get the 
+# !todo: possible bug there seems f.size issue...
+# if that is not returned properly, its not going to get the
 # parquet footer / metadata
 
 
-def byte_me(data, hex=True):
-    # some parquet files has bytes in schema/descriptions
-    # some metadata statistics contain bytes - # conver to hex for easier reading
-    if isinstance(data, dict):
-        return dict([byte_me(d, hex) for d in data.items()])
-    if isinstance(data, bytes):
-        return data.hex() if hex else data.decode('utf-8')
-    if isinstance(data, tuple):
-        return [byte_me(d, hex) for d in data]
-    if isinstance(data, list):
-        return [byte_me(d, hex) for d in data]
-    return str(data)
+class Parqu():
+    def __init__(self, FS, path, details=False) -> None:
+        self.path = path
+        self.FS = FS
+        self.details = details
+
+    @staticmethod
+    def simple_schema(file_meta: pq.FileMetaData):
+
+        columns = [{'field_name': c.name, 'physical_type': str(
+            c.physical_type), 'logical_type': str(c.logical_type)} for c in file_meta.schema]
+
+        result = {
+            'format_version': file_meta.format_version,
+            'created_by': file_meta.created_by,
+            'num_columns': file_meta.num_columns,
+            'num_rows': file_meta.num_rows,
+            'num_row_groups': file_meta.num_row_groups,
+            'schema': columns,
+        }
+        return result
+
+    @staticmethod
+    def byte_me(data, hex=True):
+        # some parquet files have bytes in schema/descriptions
+        # some metadata statistics contain bytes - # conver to hex for easier reading
+        if isinstance(data, dict):
+            return dict([Parqu.byte_me(d, hex) for d in data.items()])
+        if isinstance(data, bytes):
+            return data.hex() if hex else data.decode('utf-8')
+        if isinstance(data, tuple):
+            return [Parqu.byte_me(d, hex) for d in data]
+        if isinstance(data, list):
+            return [Parqu.byte_me(d, hex) for d in data]
+        return str(data)
+
+    @staticmethod
+    def _get_metadata(FS: pq.FileSystem, path: str, details: bool) -> dict:
+        """returns (path, status, schema and meta data)"""
+        result = {'status': 'Error', 'file_name': path,
+                  'file_size': 0, 'meta_data': None}
+        with FS.open_input_file(path) as f:
+            logger.debug(f"Opening {path}")
+            try:
+                result['file_size'] = f.size()
+                result['meta_data'] = Parqu.byte_me(pq.ParquetFile(f).metadata.to_dict(
+                )) if details else Parqu.simple_schema(pq.ParquetFile(f).metadata)
+                result['status'] = "OK"
+            except Exception as e:
+                logger.error(f"Problem with {path}")
+                logger.exception(e)
+        return result
+
+    @property
+    def meta_data(self) -> dict:
+        return Parqu._get_metadata(self.FS, self.path, self.details)
 
 
-def get_filelist(input_path: str, pattern=".parquet", recurse=False):
-    """Return filesystem type list of files"""
+    @staticmethod
+    def get_filelist(input_path: str, pattern=".parquet", recurse=False):
+        """Returns a FileSystem object and a list of FileInfo objects that matches the pattern"""
+        fs, path = FS.FileSystem.from_uri(input_path)
 
-    logger.debug(input_path)
-    fs, path = FS.FileSystem.from_uri(input_path)
-    fileinfo = fs.get_file_info([path])
+        fileinfo = fs.get_file_info([path])
+        if fileinfo[0].type == FS.FileType.NotFound or fileinfo[0].type == FS.FileType.Unknown:
+            logger.error("File not found or unknown file type")
+            raise(FileNotFoundError)
 
-    if fileinfo[0].type == FS.FileType.NotFound:
-        logger.error("File or object not found")
-        exit()
+        if fileinfo[0].type == FS.FileType.File:
+            logger.debug(f"Got a single file - {fileinfo[0].path}")
+            pass
+        elif fileinfo[0].type == FS.FileType.Directory:
+            fileinfo = fs.get_file_info(FS.FileSelector(path, recursive=recurse))
+            fileinfo = [f for f in fileinfo if pattern in f.path]
 
-    if fileinfo[0].type == FS.FileType.File:
+        logger.debug(
+            f"Finished collecting {len(fileinfo)} objects from {fs.type_name} file system")
         return fs, fileinfo
-
-    if fileinfo[0].type == FS.FileType.Directory:
-        fileinfo = fs.get_file_info(FS.FileSelector(path, recursive=recurse))
-        fileinfo = [f for f in fileinfo if pattern in f.path]
-
-    logger.info(
-        f"finished collecting {len(fileinfo)} objects from {fs.type_name} file system")
-    return fs, fileinfo
-
-
-def meta2dict(meta, details=False):
-    # schema = schema2dict(meta.schema)
-    s = meta.schema
-    columns = []
-    for i in range(len(s)):
-        columns.append({'field_name': s.column(i).name, 'physical_type': str(
-            s.column(i).physical_type), 'logical_type': str(s.column(i).logical_type)})
-
-    result = {
-        'format_version': meta.format_version,
-        'created_by': meta.created_by,
-        'num_columns': meta.num_columns,
-        'num_rows': meta.num_rows,
-        'num_row_groups': meta.num_row_groups,
-        'schema': columns,
-    }
-
-    return result
-
-
-def get_object_metadata(path) -> dict:
-    """
-    returns (path, status, schema and meta data)
-    """
-    f = FTYPE.open_input_file(path)
-
-    result = {'status': 'Error', 'file_name': path,
-              'file_size': f.size(), 'meta_data': None}
-
-    if f.size() == 0: 
-        logger.error(f"Problem with {path}")
-        return '|'.join([result['status'], result['file_name'], str(result['file_size'])])
-
-    try:
-        # meta = meta2dict(pq.ParquetFile(f).metadata)
-        # result['meta_data'] = meta
-        # result['status'] = "OK"
-        # # if DETAILS:
-        #     result['details_meta'] = byte_me(
-        #         pq.ParquetFile(f).metadata.to_dict())
-        _ = pq.ParquetFile(f).metadata
-        result['status'] = "OK"
-    except Exception as e:
-        logger.error(f"Problem with {path}")
-        logger.exception(e)
-    finally:
-        f.close()
-        return '|'.join([result['status'], result['file_name'], str(result['file_size'])])
-
-    # return result
-
-
-def dump_metadata(objs: dict, args):
-    simple = '[status, file_name, file_size, meta_data.[format_version,created_by,num_rows, num_columns, schema[].[field_name,physical_type,logical_type][]][]][]'
-    check_only = '[status, file_name, file_size ]'
-
-    with Pool(NPOOL) as p:
-        # for path, stat, meta, schema in p.imap(get_object_metadata, objs.keys()):
-        for t in p.imap(get_object_metadata, objs.keys()):
-            if args.check:
-                # pass
-                # print('|'.join([t['status'],t['file_name'],str(t['file_size'])]))
-                # line = jmespath.search(check_only, t)
-                print(t)
-            elif args.simple:
-                line = jmespath.search(simple, t)
-                print('|'.join([str(l) for l in line]))
-            else:
-                print(json.dumps(t, indent=4))
 
 
 def main(args):
-    global FTYPE, NPOOL, DETAILS
-    NPOOL = args.npool
-    DETAILS = args.details
-    path = args.path
+    FS, objs = Parqu.get_filelist(args.path, pattern=args.pat, recurse=args.recurse)
 
-    FTYPE, objs = get_filelist(path, pattern=args.pat, recurse=args.recurse)
-
-    objs = {obj.path: obj for obj in objs}
-
-    dump_metadata(objs, args)
+    for o in objs:
+        f = Parqu(FS, path=o.path, details=args.details)
+        info = f.meta_data
+        print(json.dumps(info, indent=4, sort_keys=True))
 
 
 if __name__ == "__main__":
-    """ process to extract metadata from parquet file(s).  If a directory is given
-    it will attempt to go through all the files in the directory - recursively.  
-    Make sure you specific the path at the right level as it will recursively go thru the directories.
-    This works on local, S3 and HDFS paths.  
-    """
 
-    FTYPE = ''
-    NPOOL = 16
-    DETAILS = False
+    """ Extracts metadata from parquet file(s).  If a directory is given
+    it will attempt to go through all the files in the directory - recursively if needed.  
+    Make sure you specify the path at the right level as it will recursively go thru the directories.
+
+    This works on local, S3 and HDFS paths.
+    """
 
     parser = argparse.ArgumentParser(
         description='Extract metadata from parquet files')
     parser.add_argument(
         "--path",  help="directory or file name", required=True)
     parser.add_argument(
-        "--simple",  help="simplifed output; one line per parquet file", action="store_true", required=False)
+        "--pat",  help="extension or pattern of the filename (no regex match); applicable only if --path is a directory", default=".parquet", required=False)
     parser.add_argument(
-        "--recurse",  help="simplifed output; one line per parquet file", action="store_true", default=False, required=False)
+        "--recurse",  help="recursivly list into the directory to find files", action="store_true", default=False, required=False)
     parser.add_argument(
-        "--pat",  help="extension or pattern of the filename (no regex match)", default=".parquet", required=False)
+        "--details",  help="detail info about the parquet file", action="store_true", required=False, default=False)
     parser.add_argument(
-        "--check", help="check that the file is ok by reading the metadata", action="store_true", default=False, required=False)
-    parser.add_argument(
-        "--npool", help="number of workers to start for this run", type=int, default=16, required=False)
-    parser.add_argument(
-        "--details",  help="lots of details for the parquet file", action="store_true", default=False, required=False)
+        "--log",  help="debug information", required=False, default="INFO")
 
     args = parser.parse_args()
-    logger.basicConfig(level=logger.INFO)
+    loglevel = getattr(logger, args.log.upper())
+    logger.basicConfig(level=loglevel)
     main(args)
